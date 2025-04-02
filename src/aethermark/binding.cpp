f8dbc4cef259bdeedc55a0e7043ab7e499227362
MukulWaval <mukulwaval2000@gmail.com>
2025-04-02 11:22:45 +0530

    refactor: remove Example class binding from Aethermark module

--- src/aethermark/binding.cpp
+++ src/aethermark/binding.cpp
@@ -1,5 +1,4 @@
 #include <pybind11/pybind11.h>
-#include "Example.hpp"
 #include "Node.hpp"
 #include "NodeWalker.hpp"
 
@@ -8,7 +7,6 @@ namespace py = pybind11;
 PYBIND11_MODULE(aethermark, m)
 {
     m.doc() = "Aethermark: A Python extension for Example and Node classes.";
-    Example::bind(m);
     Node::bind(m);
     NodeWalker::bind(m);
 }
